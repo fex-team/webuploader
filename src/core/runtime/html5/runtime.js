ef8756a61a8ff0fc9f99426ed352879bb9ed23ae
liaoxuezhi <liaoxuezhi@baidu.com>
2013-08-26 17:19:02 +0800

    规范化代码格式

--- src/core/runtime/html5/runtime.js
+++ src/core/runtime/html5/runtime.js
@@ -1,27 +1,31 @@
-define( 'WebUploader/core/runtime/html5/Runtime', [
-        'WebUploader/Base',
-        'WebUploader/core/Runtime' ], function( Base, Runtime ) {
+/**
+ * @fileOverview Html5Runtime
+ */
+define( 'webuploader/core/runtime/html5/runtime', [
+        'webuploader/base',
+        'webuploader/core/runtime'
+    ], function( Base, Runtime ) {
 
-    var type = 'html5';
+        var type = 'html5';
 
-    function Html5Runtime( opts ) {
-        Runtime.call( this, opts, type );
-    }
-
-    Base.inherits( Runtime, {
-        constructor: Html5Runtime,
-
-        // ---------- 原型方法 ------------
-
-        // 不需要连接其他程序，直接trigger ready
-        init: function() {
-            this.trigger( 'ready' );
+        function Html5Runtime( opts ) {
+            Runtime.call( this, opts, type );
         }
 
+        Base.inherits( Runtime, {
+            constructor: Html5Runtime,
+
+            // ---------- 原型方法 ------------
+
+            // 不需要连接其他程序，直接trigger ready
+            init: function() {
+                this.trigger( 'ready' );
+            }
+
+        } );
+
+        // 注册html5运行时。
+        Runtime.addRuntime( type, Html5Runtime );
+
+        return Html5Runtime;
     } );
-
-    // 注册html5运行时。
-    Runtime.addRuntime( type, Html5Runtime );
-
-    return Html5Runtime;
-} );
