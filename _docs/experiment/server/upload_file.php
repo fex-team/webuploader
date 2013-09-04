acc6c0ab7b8d478a22d2c87e0a690c0ab4b1c1da
Miller <chen.minliang@gmail.com>
2013-09-04 16:17:29 +0800

    增加zip测试

--- _docs/experiment/server/upload_file.php
+++ _docs/experiment/server/upload_file.php
@@ -3,6 +3,8 @@
 
 $result = array();
 
+// var_dump($_FILES);
+
 if (empty($_FILES) || $_FILES["file"]["error"] > 0) {
 	$result = array(
 		"err" => 1
@@ -16,10 +18,21 @@ else {
 		"size" => ($_FILES["file"]["size"] / 1024) ."KB",
 		"err" => 0
 	);
-	//echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-	//echo "Type: " . $_FILES["file"]["type"] . "<br />";
-	//echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-	//echo "Stored in: " . $_FILES["file"]["tmp_name"];
+
+	$filename = "";
+
+	if ($_POST['zipped']) {
+		$filename = 'files'.time().'.zip';
+	}
+	else{
+		$filename = $result[ "name" ];
+	}
+	move_uploaded_file($_FILES["file"]["tmp_name"],
+      "files/" . $filename);
+	// echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+	// echo "Type: " . $_FILES["file"]["type"] . "<br />";
+	// echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+	// echo "Stored in: " . $_FILES["file"]["tmp_name"];
 }
 
 echo json_encode($result);
