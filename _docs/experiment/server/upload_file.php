cfa70e4255ea38f58c953560b00d145103ef4e05
Miller <chen.minliang@gmail.com>
2013-09-06 14:45:12 +0800

    增加ZIP调研

--- _docs/experiment/server/upload_file.php
+++ _docs/experiment/server/upload_file.php
@@ -3,19 +3,17 @@
 
 $result = array();
 
-// var_dump($_FILES);
-
 if (empty($_FILES) || $_FILES["file"]["error"] > 0) {
 	$result = array(
 		"err" => 1
 	);
- 	// echo "Error: " . $_FILES["file"]["error"] . "<br />";
 }
 else {
 	$result = array(
 		"name" => $_FILES["file"]["name"],
 		"type" => $_FILES["file"]["type"],
 		"size" => ($_FILES["file"]["size"] / 1024) ."KB",
+		"time" => getdate(),
 		"err" => 0
 	);
 
@@ -29,10 +27,6 @@ else {
 	}
 	move_uploaded_file($_FILES["file"]["tmp_name"],
       "files/" . $filename);
-	// echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-	// echo "Type: " . $_FILES["file"]["type"] . "<br />";
-	// echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-	// echo "Stored in: " . $_FILES["file"]["tmp_name"];
 }
 
 echo json_encode($result);
