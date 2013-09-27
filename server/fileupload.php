e9bd184c0bf3e1b447ea39c8890cbbd6940038f4
liaoxuezhi <liaoxuezhi@baidu.com>
2013-09-27 10:42:22 +0800

    添加秒传demo

--- server/fileupload.php
+++ server/fileupload.php
@@ -26,15 +26,15 @@ header("Pragma: no-cache");
 
 
 // Support CORS
-header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: *");
 // other CORS headers if any...
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit; // finish preflight CORS requests here
 }
 
 
-if ( $_REQUEST[ 'debug' ] ) {
-    $random = rand(0, 2);
+if ( !empty($_REQUEST[ 'debug' ]) ) {
+    $random = rand(0, intval($_REQUEST[ 'debug' ]) );
     if ( $random === 0 ) {
         header("HTTP/1.0 500 Internal Server Error");
         exit;
@@ -49,8 +49,10 @@ if ( $_REQUEST[ 'debug' ] ) {
 // usleep(5000);
 
 // Settings
-$targetDir = ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";
-$targetDir = 'upload';
+// $targetDir = ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";
+$targetDir = 'upload_tmp';
+$uploadDir = 'upload';
+
 $cleanupTargetDir = true; // Remove old files
 $maxFileAge = 5 * 3600; // Temp file age in seconds
 
@@ -58,6 +60,11 @@ $maxFileAge = 5 * 3600; // Temp file age in seconds
 // Create target dir
 if (!file_exists($targetDir)) {
     @mkdir($targetDir);
+}
+
+// Create target dir
+if (!file_exists($uploadDir)) {
+    @mkdir($uploadDir);
 }
 
 // Get a file name
@@ -69,7 +76,15 @@ if (isset($_REQUEST["name"])) {
     $fileName = uniqid("file_");
 }
 
+$md5File = @file('md5list.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$md5File = $md5File ? $md5File : array();
+
+if (isset($_REQUEST["md5"]) && array_search($_REQUEST["md5"], $md5File ) !== FALSE ) {
+    die('{"jsonrpc" : "2.0", "result" : null, "id" : "id", "exist": 1}');
+}
+
 $filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+$uploadPath = $uploadDir . DIRECTORY_SEPARATOR . $fileName;
 
 // Chunking might be enabled
 $chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
@@ -130,6 +145,11 @@ while ($buff = fread($in, 4096)) {
 if (!$chunks || $chunk == $chunks - 1) {
     // Strip the temp .part suffix off
     rename("{$filePath}.part", $filePath);
+
+    rename($filePath, $uploadPath);
+    array_push($md5File, md5(file_get_contents($uploadPath)));
+    $md5File = array_unique($md5File);
+    file_put_contents('md5list.txt', join($md5File, "\n"));
 }
 
 // Return Success JSON-RPC response
