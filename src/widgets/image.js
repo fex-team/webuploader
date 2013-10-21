af2bb46080c2c7bf35a002bbbd8f5f800d177ea4
Miller <chen.minliang@gmail.com>
2013-10-21 16:56:19 +0800

    增加widget机制，实现模块与主程序的解耦。

---
+++ src/widgets/image.js
@@ -0,0 +1,45 @@
+/**
+ * @fileOverview 图片操作
+ * @import queue.js
+ */
+define( 'webuploader/widgets/image', [ 
+    'webuploader/base',
+    'webuploader/core/uploader' ], function( 
+        Base, Uploader ) {
+
+    var $ = Base.$;
+
+    return Uploader.register({
+
+        init: function( opts ) {
+            this.Image = this.runtime.getComponent( 'Image' );
+
+            if ( opts.resize ) {
+                $.extend( this.Image.defaultOptions.resize, opts.resize );
+            }
+        },
+
+        resize: function( source, callback ) {
+            this.Image.resize( source, callback );
+        },
+
+        makeThumb: function( file, cb, width, height, type, quality ) {
+            var runtime = this.runtime;
+
+            file = this.request( 'get-file', [ file ] );
+
+            // 只预览图片格式。
+            if ( !file.type.match( /^image/ ) ) {
+                cb( true );
+                return;
+            }
+
+            this.Image.makeThumbnail( file.getSource(), cb, width, height,
+                    true, type, quality );
+        }
+    }, {
+        'image-resize': 'resize',
+        'make-thumb': 'makeThumb'
+    });
+    
+} );
