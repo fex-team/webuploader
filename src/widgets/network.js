92167911698f71d26aa02429eb5702b516a9e4c2
Miller <chen.minliang@gmail.com>
2013-10-21 16:56:19 +0800

    增加widget机制，实现模块与主程序的解耦。

---
+++ src/widgets/network.js
@@ -0,0 +1,23 @@
+/**
+ * @fileOverview 组件基类。
+ * @import widget.js
+ */
+define( 'webuploader/widgets/network', [ 
+    'webuploader/base',
+    'webuploader/core/uploader' ], function( 
+        Base, Uploader ) {
+
+    var $ = Base.$;
+
+    return Uploader.register({
+        init: function() {
+            var me = this,
+                Network = me.runtime.getComponent( 'Network' );
+
+            Network.getInstance().on( 'all', function() {
+                return me.owner.trigger.apply( me.owner, arguments );
+            } );
+        }
+    });
+    
+} );
