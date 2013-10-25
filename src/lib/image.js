4af8a5a365f14d82a77d4c60720a572995e9d007
liaoxuezhi <liaoxuezhi@baidu.com>
2013-10-25 13:34:09 +0800

    调整代码

--- src/lib/image.js
+++ src/lib/image.js
@@ -2,37 +2,74 @@
  * @fileOverview Image帮助类，主要用来生成缩略图和压缩图片。
  * @import base.js, runtime/client.js
  */
-define( 'webuploader/core/image', [ 'webuploader/base',
-        'webuploader/runtime/client' ], function( Base, RuntimeClient ) {
+define( 'webuploader/lib/image', [ 'webuploader/base',
+        'webuploader/runtime/client',
+        'webuploader/core/mediator' ],
+        function( Base, RuntimeClient, Mediator ) {
     var $ = Base.$;
 
-    function Image() {
-        RuntimeClient.apply( this )
+    function Image( opts ) {
+        this.options = $.extend( {}, Image.options, opts );
+        RuntimeClient.call( this, 'Image' )
     }
+
+    Image.options = {
+        quality: 90,
+        crop: false,
+        allowMagnify: false,
+        crossOrigin: 'Anonymous',
+        preserveHeader: true,
+        metaHead: true
+    };
 
     Base.inherits( RuntimeClient, {
         constructor: Image,
 
-        load: function() {
+        load: function( src ) {
+            var me = this,
+                opts = src.getRuid ? src.getRuid() : $.extend( {}, me.options );
 
+            me.connectRuntime( opts, function() {
+                me.exec( 'load', src );
+            });
         },
 
-        resize: function() {
-
+        downsize: function( width, height ) {
+            return this.exec( 'downsize', width, height );
         },
 
-        makeThumbnail: function() {
-
+        makeThumbnail: function( width, height ) {
+            return this.exec( 'makeThumbnail', width, height );
         },
 
         getOrientation: function() {
-
+            return this.exec( 'getOrientation');
         },
 
-        getAsBlob: function() {
+        getAsBlob: function( type, quality ) {
+            return this.exec( 'toBlob', type, quality );
+        },
 
+        getMetas: function() {
+            return this.exec( 'getMetas' );
+        },
+
+        setMetas: function( val ) {
+            var me = this;
+            this.runtimeReady( function() {
+                me.exec( 'setMetas', val );
+            });
+            return true;
+        },
+
+        destroy: function() {
+            this.trigger( 'destroy' );
+            this.off();
+            this.exec( 'destroy' );
         }
     } );
+
+    Mediator.installTo( Image.prototype );
 
     return Image;
 } );
--- src/widgets/image.js
+++ src/widgets/image.js
@@ -1,38 +1,64 @@
 /**
  * @fileOverview 图片操作
- * @import base.js, core/uploader.js
+ * @import base.js, widgets/widget.js, core/uploader.js, lib/image.js
  */
 define( 'webuploader/widgets/image', [
     'webuploader/base',
-    'webuploader/core/uploader' ], function(
-        Base, Uploader ) {
+    'webuploader/core/uploader',
+    'webuploader/lib/image' ], function(
+        Base, Uploader, Image ) {
 
-    var $ = Base.$;
+    var $ = Base.$,
+        getInstance;
+
+    $.extend( Uploader.options, {
+        resize: {
+            width: 1600,
+            height: 1600,
+            quality: 90
+        }
+    } );
+
+    // 带有节流性质的创建器, 根据文件大小来节流
+    getInstance = (function( throttle ){
+        var runing = 0,
+            wating = [],
+            getInstance = function( size, opts ) {
+                var image = new Image( opts );
+
+                image.on( 'destroy', function() {
+                    runing -= size;
+                    Base.nextTick( tick );
+                });
+
+                return image;
+            },
+            tick = function() {
+                var item;
+                while ( runing < throttle && wating.length ) {
+                    item = wating.shift();
+                    runing += item[ 1 ];
+                    item[ 0 ]( getInstance( item[ 1 ], item[ 2 ] ) );
+                }
+            };
+
+        return function( source, cb, opts ) {
+            wating.push( [cb, source.size || source.length, opts] );
+            Base.nextTick( tick );
+        };
+    })( 5 * 1024 * 1024 );
 
     return Uploader.register(
         {
-            'image-resize': 'resize',
-            'make-thumb': 'makeThumb'
+            'make-thumb': 'makeThumb',
+            'before-start-transport': 'resizeImage'
         },
 
         {
+            makeThumb: function( file, cb, width, height ) {
+                var image;
 
-            init: function( opts ) {
-                this.Image = this.runtime.getComponent( 'Image' );
-
-                if ( opts.resize ) {
-                    $.extend( this.Image.defaultOptions.resize, opts.resize );
-                }
-            },
-
-            resize: function( source, callback ) {
-                this.Image.resize( source, callback );
-            },
-
-            makeThumb: function( file, cb, width, height, type, quality ) {
-                var runtime = this.runtime;
-
-                file = this.request( 'get-file', [ file ] );
+                file = this.request( 'get-file', file );
 
                 // 只预览图片格式。
                 if ( !file.type.match( /^image/ ) ) {
@@ -40,8 +66,65 @@ define( 'webuploader/widgets/image', [
                     return;
                 }
 
-                this.Image.makeThumbnail( file.getSource(), cb, width, height,
-                        true, type, quality );
+                getInstance( file.source, function( image ) {
+                    image.once( 'load', function() {
+                        file.metas = image.getMetas();
+                        file.orientation = image.getOrientation();
+                        cb( false, image.makeThumbnail( width, height ) );
+                        image.destroy();
+                    });
+                    image.once( 'error', function( reason ) {
+                        cb( reason );
+                        image.destroy();
+                    })
+                    image.load( file.source );
+                }, {
+                    allowMagnify: true,
+                    crop: true,
+                    preserveHeader:false
+                });
+            },
+
+            resizeImage: function( file ) {
+                var resize = this.options.resize,
+                    deferred;
+
+                if ( resize && (file.type === 'image/jpg' ||
+                        file.type === 'image/jpeg') && !file.resized ) {
+
+                    deferred = Base.Deferred();
+
+                    getInstance( file.source, function( image ) {
+                        image.once( 'load', function() {
+                            var ret, size;
+
+                            image.downsize( resize.width, resize.height, resize.quality );
+
+                            ret = image.getAsBlob();
+                            image.destroy();
+                            image = null;
+
+                            size = file.size;
+                            file.source = ret;
+                            file.size = ret.size;
+                            file.resized = true;
+                            file.trigger( 'resize', ret.size, size );
+                            deferred.resolve( true );
+
+                        });
+                        image.once( 'error', function( reason ) {
+                            // @todo
+                            image.destroy();
+                        });
+
+                        file.metas && image.setMetas( file.metas );
+                        image.load( file.source );
+                    }, {
+                        preserveHeader: true
+                    });
+
+                    return deferred.promise();
+                }
             }
     });
 } );
