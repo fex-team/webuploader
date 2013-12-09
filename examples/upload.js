c4422bd6d6f8ae84ccabda0e3f05fe8f58d9d6e2
2betop <2betop.cn@gmail.com>
2013-12-09 23:58:08 +0800

    optimize logic

--- examples/upload.js
+++ examples/upload.js
@@ -143,6 +143,7 @@
 
                 // 成功
                 if ( cur === 'error' || cur === 'invalid' ) {
+                    console.log( file.statusText );
                     showError( file.statusText );
                     percentages[ file.id ][ 1 ] = 1;
                 } else if ( cur === 'interrupt' ) {
