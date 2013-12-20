bf0f9bed2a49b933ef2fc7ba5226f5281d55c78a
2betop <2betop.cn@gmail.com>
2013-12-20 10:51:06 +0800

    add doc

--- src/mediator.js
+++ src/mediator.js
@@ -18,15 +18,6 @@ define([
                     handler.cb._cb === callback) &&
                     (!context || handler.ctx === context);
         });
-
-        // @todo IE8不支持filter，需要换种写法。
-        // return arr.filter(function( handler ) {
-        //     return handler &&
-        //             (!name || handler.e === name) &&
-        //             (!callback || handler.cb === callback ||
-        //             handler.cb._cb === callback) &&
-        //             (!context || handler.ctx === context);
-        // });
     }
 
     function eachEvent( events, callback, iterator ) {
@@ -59,11 +50,37 @@ define([
 
         /**
          * 绑定事件。
+         *
+         * `callback`方法在执行时，arguments将会来源于trigger的时候携带的参数。如
+         * ```javascript
+         * var obj = {};
+         *
+         * // 使得obj有事件行为
+         * Mediator.installTo( obj );
+         *
+         * obj.on( 'testa', function( arg1, arg2 ) {
+         *     console.log( arg1, arg2 ); // => 'arg1', 'arg2'
+         * });
+         *
+         * obj.trigger( 'testa', 'arg1', 'arg2' );
+         * ```
+         *
+         * 如果`callback`中，某一个方法`return false`了，则后续的其他`callback`都不会被执行到。
+         * 切会影响到`trigger`方法的返回值，为`false`。
+         *
+         * `on`还可以用来添加一个特殊事件`all`, 这样所有的事件触发都会响应到。同时此类`callback`中的arguments有一个不同处，
+         * 就是第一个参数为`type`，记录当前是什么事件在触发。此类`callback`的优先级比脚低，会再正常`callback`执行完后触发。
+         * ```javascript
+         * obj.on( 'all', function( type, arg1, arg2 ) {
+         *     console.log( type, arg1, arg2 ); // => 'testa', 'arg1', 'arg2'
+         * });
+         * ```
+         *
          * @method on
-         * @grammar on( name, fn[, context] ) => self
-         * @param  {String}   name     事件名
+         * @grammar on( name, callback[, context] ) => self
+         * @param  {String}   name     事件名，支持多个事件用空格隔开
          * @param  {Function} callback 事件处理器
-         * @param  {Object}   context  事件处理器的上下文。
+         * @param  {Object}   [context]  事件处理器的上下文。
          * @return {self} 返回自身，方便链式
          * @chainable
          * @class Mediator
@@ -95,10 +112,10 @@ define([
         /**
          * 绑定事件，且当handler执行完后，自动解除绑定。
          * @method once
-         * @grammar once( name, fn[, context] ) => self
+         * @grammar once( name, callback[, context] ) => self
          * @param  {String}   name     事件名
          * @param  {Function} callback 事件处理器
-         * @param  {Object}   context  事件处理器的上下文。
+         * @param  {Object}   [context]  事件处理器的上下文。
          * @return {self} 返回自身，方便链式
          * @chainable
          */
@@ -125,10 +142,10 @@ define([
         /**
          * 解除事件绑定
          * @method off
-         * @grammar off( name[, fn[, context] ] ) => self
-         * @param  {String}   name     事件名
-         * @param  {Function} callback 事件处理器
-         * @param  {Object}   context  事件处理器的上下文。
+         * @grammar off( [name[, callback[, context] ] ] ) => self
+         * @param  {String}   [name]     事件名
+         * @param  {Function} [callback] 事件处理器
+         * @param  {Object}   [context]  事件处理器的上下文。
          * @return {self} 返回自身，方便链式
          * @chainable
          */
@@ -156,7 +173,7 @@ define([
         /**
          * 触发事件
          * @method trigger
-         * @grammar trigger( name[, ...] ) => self
+         * @grammar trigger( name[, args...] ) => self
          * @param  {String}   type     事件名
          * @param  {*} [...] 任意参数
          * @return {Boolean} 如果handler中return false了，则返回false, 否则返回true
@@ -180,6 +197,7 @@ define([
     /**
      * 中介者，它本身是个单例，但可以通过[installTo](#WebUploader:Mediator:installTo)方法，使任何对象具备事件行为。
      * 主要目的是负责模块与模块之间的合作，降低耦合度。
+     *
      * @class Mediator
      */
     return $.extend({
