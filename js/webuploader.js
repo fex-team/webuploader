b49bb41abbe1e720cd614902dd49bb34f2f0184f
2betop <2betop.cn@gmail.com>
2014-01-06 17:22:07 +0800

    程序自动提交，源码请查看tree/master/jekyll目录

--- js/webuploader.js
+++ js/webuploader.js
@@ -450,10 +450,9 @@
         }
     
         function eachEvent( events, callback, iterator ) {
-    
             // 不支持对象，只支持多个event用空格隔开
-            (events || '').split( separator ).forEach(function( type ) {
-                iterator( type, callback );
+            $.each( (events || '').split( separator ), function() {
+                iterator( this, callback );
             });
         }
     
