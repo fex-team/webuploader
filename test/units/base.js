bbeef34ab60b95210bc49c9e51244762bb9ca4c3
liaoxuezhi <liaoxuezhi@baidu.com>
2014-03-03 17:58:37 +0800

    add some test cases

---
+++ test/units/base.js
@@ -0,0 +1,26 @@
+define([
+    'webuploader/base'
+], function( Base ) {
+
+    module('Test Base');
+
+    test( 'Test dollar', 1, function() {
+        ok( Base.$, 'ok' );
+    });
+
+    test( 'Test bindFn', 2, function() {
+        var obj = {
+                name: '123'
+            },
+
+            fn = function( arg1 ) {
+                ok( this.name, '123', 'The name should be `123`,' );
+                ok( arg1, '456', 'The value of first arg should be `456`. ');
+            },
+
+            proxyFn = Base.bindFn( fn, obj );
+
+        proxyFn('456');
+    });
+
+});
