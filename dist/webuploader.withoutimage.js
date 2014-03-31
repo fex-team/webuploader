f11638f7f975d4226143f94cf07152ef357426f5
liaoxuezhi <liaoxuezhi@baidu.com>
2014-03-31 17:06:23 +0800

    update dist files

--- dist/webuploader.withoutimage.js
+++ dist/webuploader.withoutimage.js
@@ -3566,7 +3566,7 @@
                 elem.on( 'dragleave', this.dragLeaveHandler );
                 elem.on( 'drop', this.dropHandler );
     
-                if ( !this.options.disableGlobalDnd ) {
+                if ( this.options.disableGlobalDnd ) {
                     $( document ).on( 'dragover', this.dragOverHandler );
                     $( document ).on( 'drop', this.dropHandler );
                 }
