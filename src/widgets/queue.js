8127265b22fd139a2b3712837174727e8b8cf0fc
liaoxuezhi <liaoxuezhi@baidu.com>
2014-07-10 16:44:13 +0800

    fix https://github.com/fex-team/webuploader/issues/291

--- src/widgets/queue.js
+++ src/widgets/queue.js
@@ -85,7 +85,7 @@ define([
 
         // 判断文件是否可以被加入队列
         acceptFile: function( file ) {
-            var invalid = !file || file.size < 6 || this.accept &&
+            var invalid = !file || !file.size || this.accept &&
 
                     // 如果名字中有后缀，才做后缀白名单处理。
                     rExt.exec( file.name ) && !this.accept.test( file.name );
