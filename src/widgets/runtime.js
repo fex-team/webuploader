56c127eec6f6e31745ab1e392257fd88b3555e7a
liaoxuezhi <liaoxuezhi@baidu.com>
2014-08-29 16:33:34 +0800

    udpate

--- src/widgets/runtime.js
+++ src/widgets/runtime.js
@@ -18,7 +18,6 @@ define([
      * @description 指定运行时启动顺序。默认会想尝试 html5 是否支持，如果支持则使用 html5, 否则则使用 flash.
      *
      * 可以将此值设置成 `flash`，来强制使用 flash 运行时。
-     * ```
      */
 
     return Uploader.register({
