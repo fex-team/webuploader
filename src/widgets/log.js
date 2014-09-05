050883e783bfd7eb7503c7aa49753a5e8ab1b066
liaoxuezhi <liaoxuezhi@baidu.com>
2014-09-05 15:12:11 +0800

    优化 log

--- src/widgets/log.js
+++ src/widgets/log.js
@@ -2,7 +2,7 @@
  * @fileOverview 日志组件，主要用来收集错误信息，可以帮助 webuploader 更好的定位问题和发展。
  *
  * 如果您不想要启用此功能，请在打包的时候去掉 log 模块。
- * 
+ *
  * 或者可以在初始化的时候通过 options.disableWidgets 属性禁用。
  *
  * 如：
@@ -10,7 +10,7 @@
  *     ...
  *
  *     disableWidgets: 'log',
- * 
+ *
  *     ...
  * })
  */
@@ -22,9 +22,12 @@ define([
     var $ = Base.$,
         logUrl = ' http://static.tieba.baidu.com/tb/pms/img/st.gif??',
         product = (location.hostname || location.host || 'protected').toLowerCase(),
+
+        // 只针对 baidu 内部产品用户做统计功能。
+        enable = product && /baidu/i.exec(product),
         base;
 
-    if (!product || /^(?:\d+\.\d+\.\d+\.\d+)|(localhost)$/.exec(product)) {
+    if (!enable) {
         return;
     }
 
@@ -32,7 +35,6 @@ define([
         dv: 3,
         master: 'webuploader',
         online: 1,
-        product: product,
         module: '',
         type: 0
     };
