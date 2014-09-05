f2b7b089570a9cb0d6c805fcb07d4415c25eb8b0
liaoxuezhi <liaoxuezhi@baidu.com>
2014-09-05 15:12:30 +0800

    程序自动提交，源码请查看tree/master/jekyll目录

--- js/webuploader.js
+++ js/webuploader.js
@@ -8000,7 +8000,7 @@
      * @fileOverview 日志组件，主要用来收集错误信息，可以帮助 webuploader 更好的定位问题和发展。
      *
      * 如果您不想要启用此功能，请在打包的时候去掉 log 模块。
-     * 
+     *
      * 或者可以在初始化的时候通过 options.disableWidgets 属性禁用。
      *
      * 如：
@@ -8008,7 +8008,7 @@
      *     ...
      *
      *     disableWidgets: 'log',
-     * 
+     *
      *     ...
      * })
      */
@@ -8020,9 +8020,12 @@
         var $ = Base.$,
             logUrl = ' http://static.tieba.baidu.com/tb/pms/img/st.gif??',
             product = (location.hostname || location.host || 'protected').toLowerCase(),
+    
+            // 只针对 baidu 内部产品用户做统计功能。
+            enable = product && /baidu/i.exec(product),
             base;
     
-        if (!product || /^(?:\d+\.\d+\.\d+\.\d+)|(localhost)$/.exec(product)) {
+        if (!enable) {
             return;
         }
     
@@ -8030,7 +8033,6 @@
             dv: 3,
             master: 'webuploader',
             online: 1,
-            product: product,
             module: '',
             type: 0
         };
