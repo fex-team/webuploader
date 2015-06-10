29176182c3cf8b2a681a5656521a62bd731bbbe4
2betop <liaoxuezhi@icloud.com>
2015-06-10 22:38:20 +0800

    update

--- src/dollar-third.js
+++ src/dollar-third.js
@@ -2,7 +2,8 @@
  * @fileOverview jQuery or Zepto
  */
 define(function() {
-    var $ = window.__dollar || window.jQuery || window.Zepto;
+    var req = window.require;
+    var $ = window.__dollar || window.jQuery || window.Zepto || req('jquery') || req('zepto');
 
     if ( !$ ) {
         throw new Error('jQuery or Zepto not found!');
