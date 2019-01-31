4b5a88a3324d9c49161eb453fd5efedb5a87bd4d
liaoxuezhi <liaoxuezhi@baidu.com>
2019-01-31 15:43:07 +0800

    以防万一没有找到按钮

--- src/lib/filepicker.js
+++ src/lib/filepicker.js
@@ -98,8 +98,8 @@ define([
                 height = button.outerHeight ?
                         button.outerHeight() : button.height(),
                 */
-                width = button[0].offsetWidth || button.outerWidth() || button.width(),
-                height = button[0].offsetHeight || button.outerHeight() || button.height(),
+                width = button[0] && button[0].offsetWidth || button.outerWidth() || button.width(),
+                height = button[0] && button[0].offsetHeight || button.outerHeight() || button.height(),
                 pos = button.offset();
 
             width && height && shimContainer.css({
